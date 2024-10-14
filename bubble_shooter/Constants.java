a1e234eb809b69065e92028018dba5c60a6a2882
sanyograthod3040 <144438482+sanyograthod3040@users.noreply.github.com>
2024-10-14 01:00:13 -0700

    Add files via upload

---
+++ bubble_shooter/Constants.java
@@ -0,0 +1,10 @@
+package bubble_shooter;
+
+
+public class Constants {
+	public static final int WINDOW_SIZE_X = 640;
+	public static final int WINDOW_SIZE_Y = 480;
+	public static final int FIELD_SIZE_X = 420;
+	public static final int FIELD_SIZE_Y = 480;
+	public static final int ROW_DISTANCE = 52;
+}
---
+++ bubble_shooter/Main.java
@@ -0,0 +1,13 @@
+package bubble_shooter;
+
+import java.awt.GridLayout;
+
+import javax.swing.SwingUtilities;
+
+
+public class Main {
+	public static void main(String[] args) {
+		MainFrame gameFrame = new MainFrame();
+		gameFrame.init();
+	}
+}
